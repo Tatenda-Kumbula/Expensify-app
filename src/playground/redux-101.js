140f8522bb24bec13912e7aa69ceb7c36d03419f
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2021-01-18 23:40:56 +0200

    Add files via upload

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,57 +1,64 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
-      return {
-        count: state.count + incrementBy
-      };
-    case 'DECREMENT':
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
-      return {
-        count: state.count - decrementBy
-      };
-    case 'SET':
-      return {
-        count: action.count
-      };
-    case 'RESET':
-      return {
-        count: 0
-      };
-    default:
-      return state;
-  }
-});
+// Action generators - functions that return action objects
 
-const unsubscribe = store.subscribe(() => {
-  console.log(store.getState());
-});
-
-store.dispatch({
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
   type: 'INCREMENT',
-  incrementBy: 5
+  incrementBy
 });
 
-store.dispatch({
-  type: 'INCREMENT'
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
 });
 
-store.dispatch({
+const setCount = ({ count }) => ({
+  type: 'SET',
+  count
+});
+
+const resetCount = () => ({
   type: 'RESET'
 });
 
-store.dispatch({
-  type: 'DECREMENT'
-});
-
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 10
-});
-
-store.dispatch({
-  type: 'SET',
-  count: 101
-});
+const countReducer = (state = { count: 0 }, action) => {
+    switch (action.type) {
+      case 'INCREMENT':
+        return {
+          count: state.count + action.incrementBy
+        };
+      case 'DECREMENT':
+        return {
+          count: state.count - action.decrementBy
+        };
+      case 'SET':
+        return {
+          count: action.count
+        };
+      case 'RESET':
+        return {
+          count: 0
+        };
+      default:
+        return state;
+    }
+  };
+  
+  const store = createStore(countReducer);
+  
+  const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+  });
+  
+  store.dispatch(incrementCount({ incrementBy: 5 }))
+  
+  store.dispatch(incrementCount());
+  
+  store.dispatch(resetCount());
+  
+  store.dispatch(decrementCount());
+  
+  store.dispatch(decrementCount({ decrementBy: 10 }));
+  
+  store.dispatch(setCount({ count: -100 }));
+  
