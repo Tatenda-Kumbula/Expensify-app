ce3767d51ca26b38fe373327d82bf673f8187416
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2021-01-18 23:34:51 +0200

    Add files via upload

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -7,10 +7,10 @@
    }
  };
 
-// const { name: firstName = 'Anonymous', age } = person;
-// console.log(`${firstName} is ${age}.`);
+ const { name: firstName = 'Anonymous', age } = person;
+ console.log(`${firstName} is ${age}.`);
 
-// const { city, temp: temperature } = person.location;
-// if (city && temperature) {
-//   console.log(`It's ${temperature} in ${city}.`);
-// }
+ const { city, temp: temperature } = person.location;
+ if (city && temperature) {
+   console.log(`It's ${temperature} in ${city}.`);
+}
