b7ef64bd5569eb1710b51ecd457de96cadf0d1df
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2021-01-18 23:40:10 +0200

    Add files via upload

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -15,14 +15,27 @@
  //  console.log(`It's ${temperature} in ${city}.`);
 //}
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-      // name: 'Penguin'
-    }
-  };
-  
-  const { name: publisherName = 'Self-Published' } = book.publisher;
-  
-  console.log(publisherName);
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     // name: 'Penguin'
+//   }
+// };
+
+// const { name: publisherName = 'Self-Published' } = book.publisher;
+
+// console.log(publisherName); // Penguin, Self-Published
+
+//
+// Array destructuring
+//
+
+// const address = ['8215 James Street', 'Alberton', 'Gauteng', '1426'];
+// const [, city, state = 'New York'] = address;
+// console.log(`You are in ${city} ${state}.`);
+
+const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75'];
+const [itemName, , mediumPrice] = item;
+
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
