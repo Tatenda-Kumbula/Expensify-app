d3ebeef29757d2059a7159734a3c2da8bfb520e2
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2021-01-18 23:28:56 +0200

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const path=require('path');
 
 
 module.exports = {
-  entry: './src/App.js ', 
+  entry: './src/playground/redux-101.js', 
   output:{
       path:path.join(__dirname,'public'),
       filename: 'bundle.js'
-  
+   
 },
 module : {
   rules: [{
