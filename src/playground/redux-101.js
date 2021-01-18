c3aad68ff43708f5337accdc7ed619da6aabb54c
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2021-01-18 23:34:18 +0200

    Add files via upload

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,12 +3,18 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
     case 'DECREMENT':
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
+      };
+    case 'SET':
+      return {
+        count: action.count
       };
     case 'RESET':
       return {
@@ -19,21 +25,33 @@ const store = createStore((state = { count: 0 }, action) => {
   }
 });
 
-store.dispatch({
-    type: 'INCREMENT'
-  });
-  
-  store.dispatch({
-    type: 'INCREMENT'
-  });
-  
-  store.dispatch({
-    type: 'RESET'
-  });
-  
-  store.dispatch({
-    type: 'DECREMENT'
-  });
-  
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
 
-console.log(store.getState());
+store.dispatch({
+  type: 'INCREMENT',
+  incrementBy: 5
+});
+
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+store.dispatch({
+  type: 'RESET'
+});
+
+store.dispatch({
+  type: 'DECREMENT'
+});
+
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10
+});
+
+store.dispatch({
+  type: 'SET',
+  count: 101
+});
