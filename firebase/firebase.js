fcb45d92f0a1f3287e5c13a59c054e25e297b0ce
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2021-01-28 04:28:18 +0200

    Add files via upload

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -9,3 +9,28 @@ firebase.initializeApp(config);
 firebase.database().ref().set({
   name: 'Tatenda Kumbula'
 });
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+database.ref().set({
+  name: 'Tatenda Kumbula',
+  age: 19,
+  isSingle: false,
+  location: {
+    city: 'Alberton',
+    country: 'South Africa'
+  }
+});
+
+// database.ref().set('This is my data.');
+
+database.ref('age').set(19);
+database.ref('location/city').set('Alberton');
+
+database.ref('attributes').set({
+  height: 73,
+  weight: 55
+});
+
+console.log('I made a request to change the data.');
