a34d6747c62e70cbf5d2d1e834915f18c75c8416
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2021-01-28 06:20:17 +0200

    Add files via upload

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,17 +1,21 @@
 const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      // resolve({
-      //   name: 'Tatenda',
-      //   age: 19
-      // });
-      reject('Something went wrong!');
+       resolve({
+         name: 'Tatenda',
+         age: 19
+       });
+      //reject('Something went wrong!');
     }, 5000);
   });
   
   console.log('before');
-  
+
   promise.then((data) => {
     console.log('1', data);
+  
+    return 'some data';
+  }).then((str) => {
+    console.log('does this run?', str);
   }).catch((error) => {
     console.log('error: ', error);
   });
